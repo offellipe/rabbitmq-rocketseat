fda4f0b61c6ec14e792a1f64c18afbfd080edd83
Luis Fellipe Batista Silva <luis.batista@irricontrol.com.br>
2024-10-29 08:18:06 -0300

    Aula 08 - Implementando Consumer

--- src/main/rabbitmq_configs/consumer.py
+++ src/main/rabbitmq_configs/consumer.py
@@ -1,15 +1,17 @@
 import pika
-import json
 
 
-class RabbitMQPublisher:
-    def __init__(self):
+def rabbitmq_callback(ch, method, properties, body):
+    print(body)
+
+
+class RabbitMQConsumer:
+    def __init__(self) -> None:
         self.__host = "localhost"
         self.__port = 5672
         self.__username = "guest"
         self.__password = "guest"
-        self.__exchange = "_minha_exchange"
-        self.__rounting_key = ""
+        self.__queue = "minha_queue"
         self.__channel = self.create_channel()
 
     def create_channel(self) -> None:
@@ -21,20 +23,21 @@ class RabbitMQPublisher:
                 password=self.__password
             )
         )
-        channel = pika.BlockingConnection(connection_parameters).channel()
-        print(channel)
-        return channel
 
-    def send_message(self, body: dict):
-        self.__channel.basic_publish(
-            exchange=self.__exchange,
-            routing_key=self.__rounting_key,
-            body=json.dumps(body),
-            properties=pika.BasicProperties(
-                delivery_mode=2
-            )
+        channel = pika.BlockingConnection(connection_parameters).channel()
+        channel.queue_declare(
+            queue=self.__queue,
+            durable=True
         )
 
+        channel.basic_consume(
+            queue=self.__queue,
+            auto_ack=True,
+            on_message_callback=rabbitmq_callback
+        )
 
-rabbit_mq_publisher = RabbitMQPublisher()
-rabbit_mq_publisher.send_message({"msg": "Testando meu consumidor"})
+        return channel
+
+    def start(self):
+        print("Sistema Conectado ao RabbitMQ")
+        self.__channel.start_consuming()
